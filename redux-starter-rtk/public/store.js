362b31986c08416aac2db62dd8419ef43b12d758
Jaspreet <jaspreetsingh2k20@gmail.com>
2023-03-05 22:21:15 +0530

    faults

---
+++ redux-starter-rtk/public/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "features/counter/counterSlice.js";
+export const store = configureStore({
+  reducer: counterReducer,
+});
---
+++ redux-starter-rtk/src/features/counter/Counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
+
+function Counter() {
+  const count = useState((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
